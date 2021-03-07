d25b0e05479ab57f19df111e378ad9282053cff5
tomokoba <tomokoba64@gmail.com>
2021-03-07 04:01:32 +0000

    [Add] devise,header,footer

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root "homes#top"
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  root 'homes#top'
+  resources :users, except: [:new, :create]
+  
 end
