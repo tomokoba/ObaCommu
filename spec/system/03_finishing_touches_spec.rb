a3413f31b23dcd435855dd7b4ca8ddb0df69ab74
tomokoba <tomokoba64@gmail.com>
2021-04-09 10:29:22 +0000

    [Add]rspec

---
+++ spec/system/03_finishing_touches_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe '[STEP3] 仕上げのテスト' do
+  let(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:post) { create(:post, user: user) }
+  let!(:other_post) { create(:post, user: other_user) }
+
+  describe 'サクセスメッセージのテスト' do
+    subject { page }
+
+    it 'ユーザログイン成功時' do
+      visit new_user_session_path
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      click_button 'ログインする'
+      is_expected.to have_content 'ログインしました。'
+    end
+
+    it 'ユーザログアウト成功時' do
+      visit new_user_session_path
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      click_button 'ログインする'
+      logout_link = find_all('a')[6].native.inner_text
+      logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+      click_link logout_link
+      is_expected.to have_content 'ログアウトしました。'
+    end
+  end
+end
